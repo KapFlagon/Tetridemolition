c191e2f100eece4035e169b2cc558d52cadd8440
Jack Grimes <flangojack@hotmail.com>
2021-06-29 22:14:13 +0100

    Adding a basic random piece generator, that can preview three peices across two arrays, swap between main arrays of pieces, and pop next piece.

---
+++ src/game_objects/sceneless_scripts/RandomPieceGenerator.gd
@@ -0,0 +1,92 @@
+extends Node
+
+
+class_name RandomPieceGenerator
+
+
+var _piece_container_a = []
+var _piece_container_b = []
+var _active_container
+
+
+enum _E_Containers {CONTAINER_A, CONTAINER_B}
+
+
+func _ready():
+	pass
+	
+func _init():
+	_fill_container(_piece_container_a)
+	_fill_container(_piece_container_b)
+	_active_container = _E_Containers.CONTAINER_A
+
+
+func preview_next_three_pieces():
+	match _active_container:
+		_E_Containers.CONTAINER_A:
+			if _piece_container_a.size() >= 3:
+				return _peek_from_single_container(_piece_container_a)
+			else: 
+				return _peek_from_two_containers(_piece_container_a, _piece_container_b)
+		_E_Containers.CONTAINER_B:
+			if _piece_container_b.size() >= 3:
+				return _peek_from_single_container(_piece_container_b)
+			else: 
+				return _peek_from_two_containers(_piece_container_b, _piece_container_a)
+
+
+func _peek_from_single_container(piece_container):
+	var three_pieces = []
+	if piece_container.size() >= 3:
+		var counter = 0
+		three_pieces = _peek_amount_of_pieces(piece_container, 3)
+	return three_pieces
+
+
+func _peek_from_two_containers(main_container, secondary_container):
+	var three_pieces = []
+	var primary_pieces = _peek_amount_of_pieces(main_container, main_container.size())
+	three_pieces.append_array(primary_pieces)
+	var remainder = 3 - three_pieces.size()
+	var secondary_pieces = _peek_amount_of_pieces(secondary_container, remainder)
+	three_pieces.append_array(secondary_pieces)
+	return three_pieces
+
+
+func _peek_amount_of_pieces(piece_container, amount_to_peek):
+	var pieces = []
+	var counter = 0
+	while counter < amount_to_peek:
+		var item_position = piece_container.size() - 1 - counter
+		pieces.append(piece_container[item_position])
+		counter +=1
+	return pieces
+
+
+func pop_next_piece() -> Tetromino:
+	var next_piece: Tetromino
+	match _active_container:
+		_E_Containers.CONTAINER_A:
+			next_piece = _piece_container_a.pop_back()
+			if _piece_container_a.size() < 1:
+				_active_container = _E_Containers.CONTAINER_B
+				_fill_container(_piece_container_a)
+		_E_Containers.CONTAINER_B:
+			next_piece = _piece_container_b.pop_back()
+			if _piece_container_b.size() < 1:
+				_active_container = _E_Containers.CONTAINER_A
+				_fill_container(_piece_container_b)
+	return next_piece
+
+
+func _fill_container(container_array) -> void:
+	container_array.append(I_Piece.new())
+	container_array.append(J_Piece.new())
+	container_array.append(L_Piece.new())
+	container_array.append(O_Piece.new())
+	container_array.append(S_Piece.new())
+	container_array.append(T_Piece.new())
+	container_array.append(Z_Piece.new())
+	randomize()
+	container_array.shuffle()
+
