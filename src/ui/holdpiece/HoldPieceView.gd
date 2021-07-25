2d25605bc410623bba834e2734b1a0fb3949ba13
Jack Grimes <flangojack@hotmail.com>
2021-07-25 17:37:06 +0100

    Renamed a function parameter to better reflect its usage. Caught some potential errors etc. Finalized implementing piece holding.

--- src/ui/holdpiece/HoldPieceView.gd
+++ src/ui/holdpiece/HoldPieceView.gd
@@ -7,12 +7,15 @@ var _current_held_piece setget swap_current_held_piece, get_current_held_piece
 
 func swap_current_held_piece(new_current_held_piece):
 	if (_last_held_piece == null) or (_last_held_piece != new_current_held_piece):
-		remove_child(_current_held_piece)
-		_last_held_piece = _current_held_piece
+		if _current_held_piece != null:
+			remove_child(_current_held_piece)
+			_last_held_piece = _current_held_piece
 		_current_held_piece = new_current_held_piece
+		_current_held_piece.reset_rotation_data()
 		_update_positions()
 		add_child(_current_held_piece)
 		return _last_held_piece
+
 
 func get_current_held_piece():
 	return _current_held_piece
@@ -23,8 +26,8 @@ func _update_positions() -> void:
 	_current_held_piece.position.y = 90
 
 
-func swap_permitted(new_current_held_piece) -> bool:
-	if (_last_held_piece == null) or (_last_held_piece != new_current_held_piece):
+func swap_permitted(piece_to_hold) -> bool:
+	if (_last_held_piece == null) or (_last_held_piece != piece_to_hold):
 		return true
 	else: 
 		return false
